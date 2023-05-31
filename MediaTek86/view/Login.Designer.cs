6b4d18a42c54ae45decd172a5628a15db01a57d4
Verre2OuiSki <48868686+Unarray@users.noreply.github.com>
2023-05-31 17:51:33 +0200

    Use SystemPasswordChar to hide password

--- MediaTek86/view/Login.Designer.cs
+++ MediaTek86/view/Login.Designer.cs
@@ -67,9 +67,9 @@ namespace MediaTek86
             // 
             this.textBox2.Location = new System.Drawing.Point(12, 63);
             this.textBox2.Name = "textBox2";
-            this.textBox2.PasswordChar = '*';
             this.textBox2.Size = new System.Drawing.Size(189, 20);
             this.textBox2.TabIndex = 2;
+            this.textBox2.UseSystemPasswordChar = true;
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // button1
