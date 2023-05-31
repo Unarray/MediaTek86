a8be2d58efb8a9327c1e77a708a52e02a7a5a68f
Verre2OuiSki <48868686+Unarray@users.noreply.github.com>
2023-05-31 17:40:01 +0200

    Create login menu

--- MediaTek86/view/Login.cs
+++ MediaTek86/view/Login.cs
@@ -21,5 +21,20 @@ namespace MediaTek86
         {
 
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
