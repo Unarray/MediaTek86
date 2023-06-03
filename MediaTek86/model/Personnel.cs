dd198c4596aeeadaf9c58e1301d1f999e11f3e00
Verre2OuiSki <48868686+Unarray@users.noreply.github.com>
2023-06-03 11:27:56 +0200

    Create models

---
+++ MediaTek86/model/Personnel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaTek86.model
+{
+    class Personnel
+    {
+        public Personnel(int id, int serviceId, string nom, string prenom, string tel, string mail)
+        {
+            this.id = id;
+            this.serviceId = serviceId;
+            this.nom = nom;
+            this.prenom = prenom;
+            this.tel = tel;
+            this.mail = mail;
+        }
+
+        public int id { get;  }
+        public int serviceId { get; set; }
+        public string nom { get; set; }
+        public string prenom { get; set; }
+        public string tel { get; set; }
+        public string mail { get; set; }
+    }
+}
---
+++ MediaTek86/model/Service.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaTek86.model
+{
+    class Service
+    {
+
+        public Service(int id, string nom)
+        {
+            this.id = id;
+            this.nom = nom;
+        }
+
+        public int id { get; }
+        public string nom { get; set; }
+    }
+}
