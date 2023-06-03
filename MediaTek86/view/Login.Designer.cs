971b9403fcec1ed89996bfb300cdd7ed75a128ac
Verre2OuiSki <48868686+Unarray@users.noreply.github.com>
2023-06-03 15:05:37 +0200

    Set form fixed & change btn content

--- MediaTek86/view/Login.Designer.cs
+++ MediaTek86/view/Login.Designer.cs
@@ -88,6 +88,7 @@ namespace MediaTek86.view
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Login";
             this.Text = "Se connecter";
             this.ResumeLayout(false);
