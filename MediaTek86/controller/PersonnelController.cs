59d3bffb16c63ae466024f04268fb6ede8eeb836
Unarray <48868686+Unarray@users.noreply.github.com>
2023-06-05 18:51:09 +0200

    Create `PersonnelController.cs` and add `GetPersonnels` methode

---
+++ MediaTek86/controller/PersonnelController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediaTek86.dal;
+using MediaTek86.model;
+
+namespace MediaTek86.controller
+{
+    class PersonnelController
+    {
+        private readonly PersonnelAccess personnelAccess;
+
+        public PersonnelController()
+        {
+            personnelAccess = new PersonnelAccess();
+        }
+
+        public List<Personnel> GetPersonnels()
+        {
+            return personnelAccess.GetPersonnels();
+        }
+    }
+}
