d34d233c819559f8f58d8e457a9fe7cad93947df
Verre2OuiSki <48868686+Unarray@users.noreply.github.com>
2023-06-05 18:31:39 +0200

    Create `GetPersonnels`

--- MediaTek86/dal/PersonnelAccess.cs
+++ MediaTek86/dal/PersonnelAccess.cs
@@ -16,5 +16,50 @@ namespace MediaTek86.dal
             access = Access.GetInstance();
         }
 
+        public List<Personnel> GetPersonnels()
+        {
+            List<Personnel> personnels = new List<Personnel>();
+
+            if (access == null) return personnels;
+
+            string req = @"SELECT
+                personnel.IDPERSONNEL,
+                personnel.NOM,
+                personnel.PRENOM,
+                personnel.TEL,
+                personnel.MAIL,
+                service.IDSERVICE,
+                service.NOM AS NOMSERVICE
+            FROM
+                personnel
+            INNER JOIN service ON service.IDSERVICE = personnel.IDSERVICE";
+
+            try
+            {
+                List<Dictionary<string, object>> records = access.Manager.ReqSelect(req);
+
+                foreach(Dictionary<string, object> row in records)
+                {
+                    personnels.Add(new Personnel(
+                        (int)row["IDPERSONNEL"],
+                        new Service(
+                            (int)row["IDSERVICE"],
+                            (string)row["NOMSERVICE"]
+                        ),
+                        (string)row["NOM"],
+                        (string)row["PRENOM"],
+                        (string)row["TEL"],
+                        (string)row["MAIL"]
+                    ));
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Environment.Exit(0);
+            }
+
+            return personnels;
+        }
     }
 }
