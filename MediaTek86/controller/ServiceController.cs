ea93c4f21f7d408107a916f381d19cf7c229f5f4
Unarray <48868686+Unarray@users.noreply.github.com>
2023-06-08 17:28:02 +0200

    Create ServiceController.cs

---
+++ MediaTek86/controller/ServiceController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediaTek86.dal;
+using MediaTek86.model;
+
+namespace MediaTek86.controller
+{
+    public class ServiceController
+    {
+        private readonly ServiceAccess serviceAccess;
+
+        public ServiceController()
+        {
+            serviceAccess = new ServiceAccess();
+        }
+
+        public List<Service> GetServices()
+        {
+            return serviceAccess.GetServices();
+        }
+    }
+}
