1b6a85a181114c0f75335173935e81d2a61e1f65
Unarray <48868686+Unarray@users.noreply.github.com>
2023-06-08 17:27:59 +0200

    Create ServiceAccess.cs

---
+++ MediaTek86/dal/ServiceAccess.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediaTek86.model;
+
+namespace MediaTek86.dal
+{
+    public class ServiceAccess
+    {
+        private readonly Access access = null;
+
+        public ServiceAccess()
+        {
+            access = Access.GetInstance();
+        }
+
+        public List<Service> GetServices()
+        {
+            List<Service> services = new List<Service>();
+
+            if (access == null) return services;
+
+            string req = "SELECT IDSERVICE, NOM FROM service;";
+
+            try
+            {
+                List<Dictionary<string, object>> records = access.Manager.ReqSelect(req);
+
+                foreach (Dictionary<string, object> row in records)
+                {
+
+                    services.Add(new Service(
+                        (int)row["IDSERVICE"],
+                        (string)row["NOM"]
+                    ));
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Environment.Exit(0);
+            }
+
+            return services;
+        }
+    }
+}
